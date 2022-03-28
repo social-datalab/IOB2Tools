f00f41a4512e3ec41f3f756eaed65ec18440626b
Susana Sotelo <sdocio@users.noreply.github.com>
2022-03-28 13:38:16 +0200

    Add code (real first commit)

--- README.md
+++ README.md
@@ -1,2 +1,289 @@
-# spacy2iob
-Script to produce a corpus with NER labels in IOB2 format.
+# Description
+
+`spacy2iob.py` produces a corpus with NER labels in IOB2 format. The source can be raw text (it relies on [spaCy](https://spacy.io/) to obtain the labels) or a gold standard in XML. It is designed to work with [this XML Schema](https://galabra.linguarum.net/corpus/schemadoc/), though it can be adapted to some extent by means of a config file.
+
+It may also produce a version of the *gold standard corpus* (in XML) labelled with [spaCy](https://spacy.io/), so the output can be used to evaluate spaCy models.
+
+# Installation
+
+## Dependencies
+
+In order to use `spacy2iob.py`, you have to install these python modules: spaCy, BeautifulSoup and lxml.
+
+```bash
+pip install spacy
+pip install bs4
+pip install lxml
+```
+
+or
+
+```bash
+pip install -r requirements.txt
+```
+
+Along with the modules, you may want to download a [spaCy pretrained model](https://spacy.io/usage/models).
+
+Example:
+
+```bash
+python3 -m spacy download pt_core_news_sm
+```
+
+# Usage
+
+It takes one or more XML or text files as input, and produces IOB2 files with the same name and extension `.iob`.
+
+1. **Raw text labelling**
+
+```bash
+python spacy2iob.py -m es_core_news_lg -f text corpus/*.txt
+```
+
+This generates an IOB2 version of the text files in `corpus`, using the spaCy pretrained model `es_core_news_lg`. The resulting files are stored in `corpus` with the same name and extension `.iob`.
+
+2. **XML with a gold standard**
+
+```bash
+python spacy2iob.py -s golden corpus_000.xml
+```
+
+It produces an IOB2 version of `corpus_000.xml` and saves it as `corpus_000.iob`. NER labels are obtained from the XML tags and spaCy is merely used for tokenization.
+
+3. **XML with labelling from spaCy**
+
+It is also possible to generate an IOB2 version of the XML files labelled with spaCy.
+
+```bash
+python spacy2iob.py -s spacy corpus/*.xml
+```
+
+This may be useful for evaluating a spaCy model against the gold standard IOB2 version obtained in section 2.
+
+XML files must tipically validate against [this XML Schema](https://galabra.linguarum.net/corpus/schemadoc/), but it can be configured to use a different set of tags (see below).
+
+With `--help` or `-h`, a helping message about usage is displayed.
+
+```txt
+$ python3 spacy2iob.py -h
+usage: spacy2iob.py [-h] [-s {spacy,golden}] [-f {text,xml}]
+                    [-m {es_core_news_lg}] [-c CONFIG] [-d] [-v]
+                    input files [input files ...]
+
+Script to convert a corpus (in XML or raw text) into IOB.
+
+positional arguments:
+  input files           Corpus files
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s {spacy,golden}, --source {spacy,golden}
+                        Source of the entity tags ("golden" = XML file with
+                        the gold standard) (default: spacy)
+  -f {text,xml}, --format {text,xml}
+                        Format of the input file with the corpus (XML or raw
+                        text) (default: xml)
+  -m MODEL, --model MODEL
+                        spaCy model (only with --source="spacy") (default:
+                        None)
+  -c CONFIG, --config CONFIG
+                        Config file (default: None)
+  -d, --dump-config     Dump a config file with default options (default:
+                        False)
+  -v, --version         show program's version number and exit
+```
+
+# Configuration
+
+Default options may be overridden by using a config file in JSON. You can obtain a config file containing the default options by calling the script with `--dump-config`.
+
+```json
+{
+    "general": {
+        "iob_separator": "\t"
+    },
+    "golden": {
+        "tags": [
+            "a",
+            "q"
+        ],
+        "attributes": {},
+        "entity_tag": "entity",
+        "entity_attr": "type",
+        "mapping": {
+            "org": "ORG",
+            "location": "LOC",
+            "misc": "MISC",
+            "person": "PER",
+            "webpage": "MISC",
+            "title": "MISC",
+            "email": "MISC"
+        }
+    },
+    "spacy": {
+        "model": "en_core_web_sm",
+        "split_sentences": false,
+        "mapping": {
+            "PER": "PER",
+            "LOC": "LOC",
+            "GPE": "LOC",
+            "ORG": "ORG",
+            "MISC": "MISC",
+            "PRODUCT": "MISC",
+            "PERSON": "PER"
+        }
+    }
+}
+```
+
+Options:
+
+- section `general`
+
+    - `iob_separator` (string): field delimiter between the token and the IOB2 label.
+
+- section `golden`: configuration regarding the processing of the gold standard in XML.
+
+    - `tags` (array): list of tags in the XML containing the textual data we want to label. Tags other than these won't be processed. If the list is empty, every tag in the XML file will be taken as input.
+    - `attributes` (object): attributes in `tags` that will be used for filtering. Only tags (in the list specified as `tags`) with these attributes (all of them) will be processed. If empty, no filter will be applied and every tag in the list will be included.
+    - `entity_tag`: XML tag storing information about entities.
+    - `entity_attr`: attribute containing the entity type.
+    - `mapping`: mapping between the XML entity types and IOB2 final labels.
+
+- section `spacy`: configuration regarding the processing of the corpus with spaCy.
+
+    - `model` (string): spaCy's pretrained model to be used. It may be overridden by the command line option `--model`.
+    - `split_sentences` (boolean): if it is set to `false`, sentence boundaries are not marked and the tokens are arranged one after the other. Otherwise, an extra newline character will be added between sentences. 
+    - `mapping` (object): mapping between spaCy and IOB2 labels.
+
+# Examples
+
+## XML gold standard
+
+Sample XML file:
+
+```xml
+<?xml version="1.0" encoding="utf-8" standalone="no" ?>
+<corpus xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="transcription.xsd">
+<metadata>
+    <participant>
+        <speaker id="162">
+            <age value="3"/>
+            <gender value="M"/>
+            <origin value="ES"/>
+        </speaker>
+    </participant>
+</metadata>
+<data>
+<text>
+  <ent enttype="person">Alejandro</ent> está viajando de <ent enttype="location">Bogotá</ent> a <ent enttype="location">Los Ángeles</ent>, en <ent enttype="location">California</ent>. Allí asistirá al congreso de la <ent enttype="org">Association for Computational Linguistics</ent>.
+</text>
+</data>
+</corpus>
+```
+
+A configuration file for processing the sample file included above could be:
+
+```json
+{
+    "general": {
+        "iob_separator": " "
+    },
+    "golden": {
+        "tags": ["text"],
+        "entity_tag": "ent",
+        "entity_attr": "enttype",
+        "mapping": {
+            "org": "ORG",
+            "location": "LOC",
+            "misc": "MISC",
+            "person": "PER"
+        }
+    }
+}
+```
+
+It process XML contents included in tag `text` and uses a blank space as IOB field separator.
+
+**Usage**
+
+- IOB2 format with labels from the XML gold standard.
+
+```python spacy2iob.py -c config.json -s golden sample.xml```
+
+Results:
+
+```
+Alejandro B_PER
+está O
+viajando O
+de O
+Bogotá B_LOC
+a O
+Los B_LOC
+Ángeles I_LOC
+, O
+en O
+California B_LOC
+. O
+
+Allí O
+asistirá O
+al O
+congreso O
+de O
+la O
+Association B_ORG
+for I_ORG
+Computational I_ORG
+Linguistics I_ORG
+. O
+```
+
+- IOB2 format with labels from spaCy.
+
+`python spacy2iob.py -c config.json sample.xml`
+
+
+## Raw text
+
+Sample text file
+
+```
+Paulo está a viajar de Lisboa para São Francisco, na Califórnia. Aí participará na conferência da Association for Computational Linguistics.
+```
+
+**Usage**
+
+```python spacy2iob.py -m pt_core_news_lg -f text sample.txt```
+
+This command will process the sample text file using the default options (we override the model to be used with `-m`).
+
+Results:
+
+```
+Paulo	B_PER
+está	O
+a	O
+viajar	O
+de	O
+Lisboa	B_LOC
+para	O
+São	B_LOC
+Francisco	I_LOC
+,	O
+na	O
+Califórnia	B_LOC
+.	O
+Aí	O
+participará	O
+na	O
+conferência	O
+da	O
+Association	B_ORG
+for	I_ORG
+Computational	I_ORG
+Linguistics	I_ORG
+.	O
+```
---
+++ spacy2iob.py
@@ -0,0 +1,344 @@
+import argparse
+import json
+import os
+import re
+import sys
+import spacy
+from bs4 import BeautifulSoup as bs
+
+
+__version = '0.1.0'
+
+fallback_cfg = {
+    "general": {
+        "iob_separator": "\t",
+    },
+    "golden": {
+        "tags": ["a", "q"],
+        "attributes": {},
+        "entity_tag": "entity",
+        "entity_attr": "type", 
+        "mapping": {
+            "org": "ORG",
+            "location": "LOC",
+            "misc": "MISC",
+            "person": "PER",
+            "webpage": "MISC",
+            "title": "MISC",
+            "email": "MISC"
+        }
+    },
+    "spacy": {
+        "model": "en_core_web_sm",
+        "split_sentences": False,
+        "mapping": {
+            "PER": "PER",
+            "LOC": "LOC",
+            "GPE": "LOC",
+            "ORG": "ORG",
+            "MISC": "MISC",
+            "PRODUCT": "MISC",
+            "PERSON": "PER"
+        }
+    },
+}
+
+
+class Config:
+    def __init__(self, conffile=None):
+        if conffile is not None:
+            self._settings = self._parse_config(conffile)
+        else:
+            self._settings = {}
+
+    def _parse_config(self, conffile):
+        if not os.path.exists(conffile):
+            raise FileNotFoundError(
+                "Config file {!r} doesn't exist.".format(conffile))
+
+        with open(conffile) as f:
+            return json.load(f)
+
+    def get_property(self, section, property_name):
+        fallback = fallback_cfg[section][property_name]
+
+        if section in self._settings:
+            return self._settings[section].get(property_name, fallback)
+
+        return fallback
+
+    @staticmethod
+    def dump():
+        return json.dumps(fallback_cfg, indent=4)
+
+
+class Spacy:
+    def __init__(self, separator="\t", mapping={}, model=None, segment=False):
+        self._sep = separator
+        self._mapping = mapping
+        self._segment = segment
+
+        try:
+            self.nlp = spacy.load(model)
+        except Exception as err:
+            print(f'Unable to load spaCy model:\n{err}', file=sys.stderr)
+            sys.exit(1)
+
+    def __call__(self, text):
+        return self.nlp(text)
+
+    def tokenizer(self, text):
+        return list(self.nlp(
+            text,
+            disable=[
+                "ner", "tok2vec", "tagger", "parser",
+                "attribute_ruler", "lemmatizer"])
+            )
+
+    def token_to_iob(self, token):
+        iob = token.ent_iob_
+        enttype = self._mapping.get(token.ent_type_, token.ent_type_)
+        iob_tag = iob if iob == 'O' else f'{iob}_{enttype}'
+
+        return f'{token.text}{self._sep}{iob_tag}'
+
+    def span_to_iob(self, doc):
+        tokens = []
+        for sentence in doc.sents:
+            for token in sentence:
+                tokens.append(self.token_to_iob(token))
+            if self._segment:
+                tokens[-1] = tokens[-1] + '\n'
+        return tokens
+
+    @staticmethod
+    def get_installed_models():
+        return spacy.util.get_installed_models() or ["No models available"]
+
+
+class _DumpConfigAction(argparse.Action):
+    def __init__(self,
+                 option_strings,
+                 dest,
+                 default=argparse.SUPPRESS,
+                 help=None):
+        super(_DumpConfigAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            default=default,
+            nargs=0,
+            help=help)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, True)
+        print(Config.dump())
+        parser.exit()
+
+
+def parse_args():
+    description = "Script to convert a corpus (in XML or raw text) into IOB."
+
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '-s',
+        '--source',
+        required=False,
+        default='spacy',
+        type=str,
+        choices=['spacy', 'golden'],
+        help='Source of the entity tags ("golden" = XML file with the gold standard)',  # noqa
+    )
+    parser.add_argument(
+        '-f',
+        '--format',
+        required=False,
+        default='xml',
+        choices=['text', 'xml'],
+        help='Format of the input file with the corpus (XML or raw text)'
+    )
+    parser.add_argument(
+        '-m',
+        '--model',
+        required=False,
+        choices=Spacy.get_installed_models(),
+        help='spaCy model (only with --source="spacy")'
+    )
+    parser.add_argument(
+        '-c',
+        '--config',
+        type=str,
+        required=False,
+        help='Config file'
+    )
+    parser.add_argument(
+        '-d',
+        '--dump-config',
+        dest='dump',
+        default=False,
+        action=_DumpConfigAction,
+        help='Dump a config file with default options',
+    )
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version=f'%(prog)s {__version}'
+    )
+    parser.add_argument(
+        'file',
+        metavar='input files',
+        type=str,
+        nargs='+',
+        help='Corpus files'
+    )
+    return parser.parse_args()
+
+
+def find_entities(entities, doc):
+    def sequence_in(entity, target, idx):
+        seq = [token.text for token in nlp.tokenizer(entity)]
+        for i in range(len(target[idx:]) - len(seq) + 1):
+            if seq == target[i+idx:i+idx+len(seq)]:
+                return(i+idx, i+idx+len(seq))
+        raise Exception(f"Entity {seq} not found in: {target}")
+
+    tokens = [token.text for token in doc]
+
+    for ent in entities:
+        entity, enttype, ent_idx = ent
+        coords = sequence_in(entity, tokens, ent_idx)
+
+        for idx, i in enumerate(range(coords[0], coords[1])):
+            tokens[i] = "{}{}{}_{}".format(
+                tokens[i],
+                IOB_separator,
+                "B" if idx == 0 else "I", enttype
+            )
+
+    for i in range(len(tokens)):
+        if not re.search(r'{}[BI]_'.format(IOB_separator), tokens[i]):
+            tokens[i] = "{}{}{}".format(tokens[i], IOB_separator, "O")
+        if doc[i].is_sent_end and segment:
+            tokens[i] = tokens[i] + '\n'
+
+    return tokens
+
+
+def get_entity_indexes(turn):
+    indexes = []
+    turn_copy = bs(re.sub(r'\s+', ' ', str(turn)), 'lxml-xml')
+
+    entities = turn_copy.find_all(entity_tag)
+    for e in entities:
+        entity_tokens = [t.text for t in nlp.tokenizer(e.text)]
+        e.string = re.sub(
+            r'^{}'.format(entity_tokens[0]),
+            entity_tokens[0] + 'PART-ENTITY',
+            e.text
+        )
+
+    tokens = [
+        t.text for t in nlp(re.sub(r'\s+', ' ', turn_copy.text.strip()))
+    ]
+    for i, token in enumerate(tokens):
+        if token.endswith('PART-ENTITY'):
+            indexes.append(i)
+        elif re.match(r'PART-ENTITY', token):
+            indexes.append(i - 1)
+
+    return indexes
+
+
+def get_iob_tokens_from_xml(corpusfile):
+    iob_tokens = []
+    data = bs(corpusfile, 'lxml-xml')
+
+    for turn in data.find_all(xmltags, attrs=attrs):
+        entities = []
+
+        if turn.text is '':
+            continue
+        offsets = get_entity_indexes(turn)
+        for i, entity in enumerate(turn.find_all(entity_tag)):
+            if args.source == 'golden':
+                try:
+                    entities.append(
+                        (
+                            entity.text,
+                            mapping.get(
+                                entity[entity_attr],
+                                entity[entity_attr]
+                            ),
+                            offsets[i]
+                        )
+                    )
+                except KeyError as err:
+                    print(
+                        f"Key {err} not found in tag {entity_tag}: {entity}",
+                        file=sys.stderr
+                    )
+                    sys.exit(2)
+            else:
+                entity.unwrap()
+
+        doc = nlp(re.sub(r'\s+', ' ', turn.text.strip()))
+        if args.source == 'golden':
+            iob_tokens.extend(find_entities(entities, doc))
+        else:
+            iob_tokens.extend(nlp.span_to_iob(doc))
+
+    return iob_tokens
+
+
+def get_iob_tokens_from_text(corpusfile):
+    iob_tokens = []
+    for line in corpusfile:
+        doc = nlp(line.strip())
+        iob_tokens.extend(nlp.span_to_iob(doc))
+    return iob_tokens
+
+
+args = parse_args()
+cfg = Config(args.config)
+if args.dump:
+    cfg.dump()
+    sys.exit(0)
+
+xmltags = cfg.get_property('golden', 'tags')
+attrs = cfg.get_property('golden', 'attributes')
+entity_tag = cfg.get_property('golden', 'entity_tag')
+entity_attr = cfg.get_property('golden', 'entity_attr')
+
+mapping = cfg.get_property(args.source, 'mapping')
+IOB_separator = cfg.get_property('general', 'iob_separator')
+segment = cfg.get_property('spacy', 'split_sentences')
+
+nlp = Spacy(
+    separator=IOB_separator,
+    mapping=mapping,
+    model=args.model or cfg.get_property('spacy', 'model'),
+    segment=segment,
+)
+
+for ifile in args.file:
+    ofile = ".".join([os.path.splitext(ifile)[0], "iob"])
+    try:
+        fout = open(ofile, "w")
+    except Exception as e:
+        print(f"Unable to write: {e}")
+        sys.exit(1)
+
+    iob_tokens = []
+    with open(ifile) as corpusfile:
+        print(f"Processing {ifile}", file=sys.stderr)
+
+        if args.format == 'text':
+            iob_tokens = get_iob_tokens_from_text(corpusfile)
+        elif args.format == 'xml':
+            iob_tokens = get_iob_tokens_from_xml(corpusfile)
+
+    print('\n'.join(iob_tokens).strip(), file=fout)
+    fout.close()
